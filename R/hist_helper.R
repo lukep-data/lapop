83f08ad554d28bb00a410ace2e4d4bce6025c2cc
Luke P <luke.plutowski@gmail.com>
2025-03-20 08:45:51 -0600

    add hist and stack helper

---
+++ R/hist_helper.R
@@ -0,0 +1,102 @@
+######################################################
+
+# Histogram Graph Pre-Processing #
+
+######################################################
+
+#' Bar/Histogram Graphs
+#'
+#' This function creates dataframes which can then be input in to ggplot for
+#' creating a histogram/bar plot.
+#'
+#' @param data Dataframe.  The data that should be analyzed.
+#' @param outcome Character. Outcome variable of interest.
+#' @param filesave Character.  Path and file name to save the dataframe as csv.
+#' @param cfmt Character. Changes the format of the numbers displayed above the bars.
+#' Uses sprintf string formatting syntax. Default is whole numbers.
+#' @param sort Character. On what value the bars are sorted.
+#' Options are "y" (for the value of the outcome variable), "xv" (default; for
+#' the underlying values of the x variable), "xl" (for the labels of the x variable,
+#' i.e., alphabetical).
+#' @param order Character.  How the bars should be sorted.  Options are "hi-lo"
+#' or "lo-hi" (default).
+#' @param keep_nr Logical.  If TRUE, will convert "don't know" (missing code .a)
+#' and "no response" (missing code .b) into valid data (value = 99).
+#' The default is to examine valid responses only.  Default: FALSE.
+#'
+#'
+#' @return Returns a data frame, with data formatted for visualization by lapop_hist
+#'
+#' @examples
+#'
+#' \dontrun{hist_helper(jam23,
+#' outcome = "aoj11",
+#' sort = "xv",
+#' order = "hi-lo")}
+#'
+#'@export
+#'@import dplyr
+#'@import srvyr
+#'
+#'@author Shashwat Dhar \email{shashwat.dhar@@vanderbilt.edu},
+#'Luke Plutowski, \email{luke.plutowski@@vanderbilt.edu}
+
+
+hist_helper <- function(data,
+                     outcome,
+                     filesave = "",
+                     cfmt = "",
+                     sort = "xv",
+                     order = "lo-hi",
+                     keep_nr = FALSE) {
+
+  # If keep_nr is TRUE, convert don't knows (NA(a)) and no answers (NA(b)) to
+  # non-NA data (a value of 99).
+  if (keep_nr) {
+    data = data %>%
+      mutate(!!sym(outcome) := case_when(
+        na_tag(!!sym(outcome)) %in% c("a", "b") ~ 99,
+        TRUE ~ as.numeric(!!sym(outcome))
+      ))
+  }
+
+  # Create the data frame
+  hist_df = data %>%
+    drop_na(outcome) %>%
+    group_by(across(outcome)) %>%  #
+    summarise(n = n()) %>%
+    rename(cat = 1) %>%
+    mutate(
+      prop = prop.table(n) * 100,
+      proplabel = if (cfmt != "") {
+        sprintf(cfmt, round(prop))
+      } else {
+        sprintf("%.0f%%", round(prop))
+      },
+      cat = (haven::as_factor(cat))
+    ) %>%
+    select(-n)
+
+  # Apply sorting
+  if (sort == "y") {
+    hist_df = hist_df %>%
+      arrange(if (order == "lo-hi") prop else desc(prop))
+  }
+
+  if (sort == "xv") {
+    hist_df = hist_df %>%
+      arrange(if (order == "lo-hi") match(cat, levels(cat)) else desc(match(cat, levels(cat))))
+  }
+
+  if (sort == "xl") {
+    hist_df = hist_df %>%
+      arrange(if (order == "lo-hi") as.character(cat) else desc(as.character(cat)))
+  }
+
+  if (filesave != "") {
+    write.csv(hist_df, filesave)
+  }
+
+  # Return the data frame
+  return(hist_df)
+}
---
+++ R/stack_helper.R
@@ -0,0 +1,163 @@
+########################################
+
+# Stacked Bar Graph Pre-Processing
+
+########################################
+
+#' Stacked Bar Graph Pre-Processing
+#'
+#' This function creates dataframes which can then be used to create stacked
+#' bar plots.
+#' If "by" is specified, the function shows the distribution of the outcome variable
+#' by distinct values of a secondary variable.  If "by" is not provided, it will show
+#' the distribution of all specified outcome variables.
+#'
+#' @param data  The data that should be analyzed.
+#' @param outcome Character vector. Vector of variable(s) be plotted.
+#' @param by Character. Grouping variable.  If by is provided, function will break
+#' down the outcome variable by distinct values of the "by" variable (instead of
+#' showing distribution of multiple outcome variables).
+#' @param sort Character. On what value the bars are sorted. Options are "xv"
+#' (default; sort on the underlying values of the value labels), "xl" (on the value
+#' labels themselves, i.e. alphabetically), or "y" (on the proportions of the
+#' outcome variable(s)).
+#' @param order Character. How the bars should be sorted. Options are "lo-hi"
+#' (default) or "hi-lo".
+#' @param filesave Character. Path and file name to save the dataframe as csv.
+#' @param keep_nr Logical. If TRUE, will convert "don't know" (missing code .a)
+#' and "no response" (missing code .b) into valid data (value = 99) and use them
+#' in the denominator when calculating percentages.  The default is to examine
+#' valid responses only.  Default: FALSE.
+#'
+#' @return Returns a data frame, with data formatted for visualization by lapop_stack
+#'
+#' @examples
+#'
+#' \dontrun{lapop_stack(data = gm, outcome = c("countfair1", "countfair3"))}
+#'
+#'@export
+#'@import dplyr
+#'@import srvyr
+#'@import purrr
+#'
+#'@author Luke Plutowski & Robert Vidigal
+
+# # -----------------------------------------------------------------------
+# stack_helper
+# # -----------------------------------------------------------------------
+stack_helper <- function(data,
+                       outcome,
+                       by = NULL,
+                       sort = "xv",
+                       order = "lo-hi",
+                       filesave = "",
+                       keep_nr = FALSE) {
+
+  if (!is.null(by)) {
+    # Exclude NA values from 'by' before looping
+    valid_values <- sort(unique(data[[by]][!is.na(data[[by]])]))
+
+    # Loop over all values of "by" variable - create a separate column for each value
+    for (value in valid_values) {
+      if (any(!is.na(data[[outcome]][data[[by]] == value]))) {
+
+        column_name <- paste0("x_by_", value)
+
+        data[[column_name]] <- replace(
+          data[[outcome]],
+          data[[by]] != value,
+          NA
+        )
+
+        # Extract value label and assign it to newly created variables
+        label_value <- names(which(attributes(data[[by]])$labels == value))
+
+        if (length(label_value) == 0) {
+          label_value <- as.character(value)  # Fallback if no label is found
+        }
+
+        attributes(data[[column_name]])$label <- label_value
+      }
+    }
+
+    # Update outcome list to include the newly created "x_by_*" variables
+    outcome <- grep("x_by", names(data), value = TRUE)
+  }
+
+  # Helper function to handle a single variable
+  process_outcome <- function(data, outcome) {
+    # Handle `keep_nr` logic
+    if (keep_nr) {
+      data <- data %>%
+        mutate(!!sym(outcome) := case_when(
+          na_tag(!!sym(outcome)) %in% c("a", "b") ~ 99, # Replace "NA(a)" and "NA(b)" with 99
+          TRUE ~ as.numeric(!!sym(outcome))       # Keep other values unchanged
+        ))
+    }
+
+    # Perform proportion calculations
+    stack <- data %>%
+      drop_na(outcome) %>%
+      group_by(across(as_factor(outcome))) %>%
+      summarise(
+        n = n()
+      ) %>%
+      rename(vallabel = 1) %>%
+      mutate(
+        varlabel = if (!is.null(attributes(data[[outcome]])$label)) {
+          attributes(data[[outcome]])$label
+        } else {
+          outcome
+        },
+        prop = n/sum(n),
+        prop = prop * 100, # Convert to percentage
+        proplabel = sprintf("%.0f%%", prop)
+      ) %>%
+      ungroup() %>%
+      dplyr::select(varlabel, vallabel, prop, proplabel)
+
+    # Sorting logic
+    stack <- stack %>%
+      {
+        if (sort == "y") {
+          if (order == "hi-lo") {
+            arrange(., desc(prop))
+          } else if (order == "lo-hi") {
+            arrange(., prop)
+          } else {
+            .
+          }
+        } else if (sort == "xv") {
+          if (order == "hi-lo") {
+            arrange(., desc(vallabel))
+          } else if (order == "lo-hi") {
+            arrange(., vallabel)
+          } else {
+            .
+          }
+        } else if (sort == "xl") {
+          if (order == "hi-lo") {
+            arrange(., desc(as.character(vallabel)))
+          } else if (order == "lo-hi") {
+            arrange(., as.character(vallabel))
+          } else {
+            .
+          }
+        } else {
+          .
+        }
+      }
+
+    return(stack)
+  }
+
+  # Apply the purrr helper function to all outcomes and combine the results
+  results <- map_dfr(outcome, ~ process_outcome(data, .x))
+
+  # Save to file if required
+  if (filesave != "") {
+    write.csv(results, filesave, row.names = FALSE)
+  }
+
+  return(results)
+}
